8a4586b3fb26857c188165146d68feaace818672
Sub_Per <118508299+nsubagya@users.noreply.github.com>
2024-09-21 06:11:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import pandas as pd
 import folium
 
 # Load the model
-model = load_model(r"https://github.com/nsubagya/crime_data_app/blob/main/final_model.pkl")
+model = load_model("final_model")
 
 # Load the data
-data_2023 = pd.read_csv(r"https://github.com/nsubagya/crime_data_app/blob/main/data_after_2023.csv")
+data_2023 = pd.read_csv("data_after_2023.csv")
 
 # Create a dictionary for crime codes and their descriptions
 crime_codes = dict(zip(data_2023['Crm Cd'], data_2023['Crm Cd Desc']))
